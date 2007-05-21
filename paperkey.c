560c21a9f4fd26378ceeec6b881f3a679c307fdb
David Shaw <dshaw@jabberwocky.com>
2007-05-21 21:52:57 +0000

    Can now restore a primary (only) key

--- paperkey.c
+++ paperkey.c
@@ -62,7 +62,7 @@ extract(FILE *input)
   int offset;
   unsigned char fingerprint[20];
 
-  packet=parse(input,5,0);\
+  packet=parse(input,5,0);
   if(!packet)
     {
       fprintf(stderr,"Unable to find secret key packet\n");
@@ -89,7 +89,7 @@ extract(FILE *input)
 
   output_bytes(packet->buf,1);
   output_bytes(fingerprint,20);
-  output_length(packet->len-offset);
+  output_length16(packet->len-offset);
   output_bytes(&packet->buf[offset],packet->len-offset);
 
   free_packet(packet);
@@ -112,7 +112,7 @@ extract(FILE *input)
 
       output_bytes(packet->buf,1);
       output_bytes(fingerprint,20);
-      output_length(packet->len-offset);
+      output_length16(packet->len-offset);
       output_bytes(&packet->buf[offset],packet->len-offset);
 
       free_packet(packet);
@@ -131,15 +131,47 @@ extract(FILE *input)
 static void
 restore(FILE *pubring,FILE *secrets)
 {
-  struct packet *packet;
-  unsigned char fpr[20];
+  struct packet *secret;
 
-  packet=read_secrets_file(secrets);
-  if(packet)
+  output_type=RAW;
+
+  secret=read_secrets_file(secrets);
+  if(secret)
     {
-      output_start(fpr);
-      output_bytes(packet->buf,packet->len);
+      struct packet *pubkey;
+      unsigned char ptag;
+      size_t sidx,secretlen;
+
+      output_start(NULL);
+
+      /* Pull out the fingerprint */
+      if(secret->len<21)
+	goto fail;
+
+      if(secret->buf[0]!=4)
+	goto fail;
+
+      pubkey=find_pubkey(pubring,&secret->buf[1]);
+
+      sidx=21;
+
+      secretlen=secret->buf[sidx++]<<8;
+      secretlen|=secret->buf[sidx++];
+
+      /* New-style secret key */
+      ptag=0xC5;
+      output_bytes(&ptag,1);
+      output_length(pubkey->len+secretlen);
+      output_bytes(pubkey->buf,pubkey->len);
+      output_bytes(&secret->buf[23],secretlen);
+
+      sidx+=secretlen;
     }
+
+  return;
+
+ fail:
+  fprintf(stderr,"Unable to read secrets file\n");
 }
 
 int

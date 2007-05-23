113ec0be7592c21f22dd51300e0959ddd5880e39
David Shaw <dshaw@jabberwocky.com>
2007-05-23 02:17:24 +0000

    make distcheck works now

--- checks/roundtrip.sh
+++ checks/roundtrip.sh
@@ -2,6 +2,6 @@
 
 # $Id$
 
-../paperkey --secret-key papertest.sec --output papertest.txt || exit 1
-../paperkey --secrets papertest.txt --pubring papertest.pub --output regen.pgp || exit 1
-cmp regen.pgp verify.pgp
+../paperkey --secret-key ${srcdir}/papertest.sec --output papertest.txt || exit 1
+../paperkey --secrets papertest.txt --pubring ${srcdir}/papertest.pub --output regen.pgp || exit 1
+cmp ./regen.pgp ${srcdir}/verify.pgp

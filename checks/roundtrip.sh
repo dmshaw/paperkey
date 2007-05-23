81e31446fc455576b2e09ff94548161d96071f3e
David Shaw <dshaw@jabberwocky.com>
2007-05-23 02:06:54 +0000

    tweaks

--- checks/roundtrip.sh
+++ checks/roundtrip.sh
@@ -2,6 +2,6 @@
 
 # $Id$
 
-../paperkey --secret-key papertest.sec --output papertest.txt
-../paperkey --secrets papertest.txt --pubring papertest.pub --output regen.pgp
+../paperkey --secret-key papertest.sec --output papertest.txt || exit 1
+../paperkey --secrets papertest.txt --pubring papertest.pub --output regen.pgp || exit 1
 cmp regen.pgp verify.pgp

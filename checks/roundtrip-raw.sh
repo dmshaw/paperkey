d4de9fa0f462bdf58bb9037b2a52e1c78b098788
David Shaw <dshaw@jabberwocky.com>
2007-09-21 21:27:49 +0000

    Use the auto on readback

--- checks/roundtrip-raw.sh
+++ checks/roundtrip-raw.sh
@@ -3,5 +3,5 @@
 # $Id$
 
 ../paperkey --secret-key ${srcdir}/papertest.sec --output-type raw --output papertest.bin || exit 1
-../paperkey --secrets papertest.bin --input-type raw --pubring ${srcdir}/papertest.pub --output regen.pgp || exit 1
+../paperkey --secrets papertest.bin --pubring ${srcdir}/papertest.pub --output regen.pgp || exit 1
 cmp ./regen.pgp ${srcdir}/verify.pgp

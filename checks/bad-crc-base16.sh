fa8bab176b1d1310a528fb704dca6afba00d6183
David Shaw <dshaw@jabberwocky.com>
2007-09-23 04:01:59 +0000

    Better test that doesn't use GNU head

--- checks/bad-crc-base16.sh
+++ checks/bad-crc-base16.sh
@@ -2,6 +2,5 @@
 
 # $Id$
 
-../paperkey --secret-key ${srcdir}/papertest.sec | head -n -1 > papertest.txt || exit 1
-../paperkey --secrets papertest.txt --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
+../paperkey --secrets ${srcdir}/corrupt-base16.txt --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
 exit 0
--- checks/bad-crc-raw.sh
+++ checks/bad-crc-raw.sh
@@ -2,6 +2,5 @@
 
 # $Id$
 
-../paperkey --secret-key ${srcdir}/papertest.sec --output-type raw | head -c -1 > papertest.bin || exit 1
-../paperkey --secrets papertest.bin --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
+../paperkey --secrets ${srcdir}/corrupt-raw.bin --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
 exit 0

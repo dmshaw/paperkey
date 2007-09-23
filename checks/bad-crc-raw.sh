3554e277273642f4508085cc7d548625eff561f1
David Shaw <dshaw@jabberwocky.com>
2007-09-23 02:31:03 +0000

    papertest.bin, not papertest.raw

--- checks/bad-crc-raw.sh
+++ checks/bad-crc-raw.sh
@@ -2,6 +2,6 @@
 
 # $Id$
 
-../paperkey --secret-key ${srcdir}/papertest.sec --output-type raw | head -c -1 > papertest.raw || exit 1
-../paperkey --secrets papertest.raw --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
+../paperkey --secret-key ${srcdir}/papertest.sec --output-type raw | head -c -1 > papertest.bin || exit 1
+../paperkey --secrets papertest.bin --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
 exit 0

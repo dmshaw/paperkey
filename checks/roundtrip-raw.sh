46adad971458a798e203bf8ec65d6bc897494754
David Shaw <dshaw@jabberwocky.com>
2012-11-29 17:16:18 -0500

    Add RSA tests

--- checks/roundtrip-raw.sh
+++ checks/roundtrip-raw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for type in dsaelg ecc ; do
+for type in rsa dsaelg ecc ; do
     ../paperkey --secret-key ${srcdir}/papertest-${type}.sec --output-type raw --output papertest-${type}.bin || exit 1
     ../paperkey --secrets papertest-${type}.bin --pubring ${srcdir}/papertest-${type}.pub --output regen.pgp || exit 1
     cmp ./regen.pgp ${srcdir}/papertest-${type}.sec || exit 1

9274bf22940f1ad951e8dd57689aa96e9e65788e
David Shaw <dshaw@jabberwocky.com>
2017-11-17 09:42:27 -0500

    Cleaner handling of the case when paperkey intentionally fails as part of a test (catches cores)

--- checks/bad-crc-raw.sh
+++ checks/bad-crc-raw.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-../paperkey --secrets ${srcdir}/corrupt-raw.bin --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null && exit 1
+../paperkey --secrets ${srcdir}/corrupt-raw.bin --pubring ${srcdir}/papertest.pub --output regen.pgp 2>/dev/null
+if test $? -ne 1 ; then
+    exit 1
+fi
+
 exit 0

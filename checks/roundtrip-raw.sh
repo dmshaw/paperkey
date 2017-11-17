4266ab7fb981a3814ee4a7074a7f2e3307a37ab9
David Shaw <dshaw@jabberwocky.com>
2017-11-17 10:03:28 -0500

    Use a different output filename for each test to avoid problems when running the selftests with make -j (Debian bug 881792)

--- checks/roundtrip-raw.sh
+++ checks/roundtrip-raw.sh
@@ -2,7 +2,7 @@
 
 for type in rsa dsaelg ecc eddsa ; do
     ../paperkey --secret-key ${srcdir}/papertest-${type}.sec --output-type raw --output papertest-${type}.bin || exit 1
-    ../paperkey --secrets papertest-${type}.bin --pubring ${srcdir}/papertest-${type}.pub --output regen.pgp || exit 1
-    cmp ./regen.pgp ${srcdir}/papertest-${type}.sec || exit 1
+    ../paperkey --secrets papertest-${type}.bin --pubring ${srcdir}/papertest-${type}.pub --output regen-raw-${type}.pgp || exit 1
+    cmp ./regen-raw-${type}.pgp ${srcdir}/papertest-${type}.sec || exit 1
     /bin/echo -n "$type "
 done
--- checks/roundtrip.sh
+++ checks/roundtrip.sh
@@ -2,7 +2,7 @@
 
 for type in rsa dsaelg ecc eddsa ; do
     ../paperkey --secret-key ${srcdir}/papertest-${type}.sec --output papertest-${type}.txt || exit 1
-    ../paperkey --secrets papertest-${type}.txt --pubring ${srcdir}/papertest-${type}.pub --output regen.pgp || exit 1
-    cmp ./regen.pgp ${srcdir}/papertest-${type}.sec || exit 1
+    ../paperkey --secrets papertest-${type}.txt --pubring ${srcdir}/papertest-${type}.pub --output regen-${type}.pgp || exit 1
+    cmp ./regen-${type}.pgp ${srcdir}/papertest-${type}.sec || exit 1
     /bin/echo -n "$type "
 done
